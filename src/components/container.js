2ddfe04c6859837dcc67c842da152154d260c10b
atinachang <atinachang@gmail.com>
2021-03-14 21:35:45 -0400

    remove boiler css files

--- src/components/container.js
+++ src/components/container.js
@@ -1,6 +1,5 @@
 import React from "react";
-import containerStyles from "./container.module.css";
 
 export default function Container({ children }) {
-    return <div className={containerStyles.container}>{children}</div>
+    return <div className="container">{children}</div>
 }
