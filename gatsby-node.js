f93a0c92e6be2e75fd20884d4840bc463065da39
atinachang <atinachang@gmail.com>
2021-04-01 22:28:34 -0400

    single film page data rendering and basic styling

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,30 @@
 exports.createPages = async ({ actions: { createPage }, graphql }) => {
 	
 	const result = await graphql(
-		`
-		query getFilm ($film_title: String){
-			hasura{
-tnff_films(where: {
-    title: {
-      _eq: $film_title
-    }
-  }) {
-    title
-		synopsis
-  }
-}
-}`
-	)
+`
+	query getFilm ($film_title: String){
+		hasura{
+		tnff_films(where: {
+		title: {_eq: $film_title}	})
+			{
+				title
+				synopsis
+				tnff_year
+				director_bio
+				director
+				duration_mins
+				language
+				release_date
+				location
+				other_info
+				film_img_url
+				category
+				director_img_url
+			}
+		}
+	}
+`
+)
 
 
 	const filmTemplate = require.resolve('./src/templates/FilmPage.js')
--- src/templates/FilmPage.js
+++ src/templates/FilmPage.js
@@ -1,15 +1,44 @@
-import React, { Fragment } from "react";
+import React from 'react'
+import {BiWorld} from 'react-icons/bi'
+import {BsCalendar, BsPersonFill }from 'react-icons/bs'
+import {AiOutlineClockCircle} from 'react-icons/ai'
+import { FaLanguage } from 'react-icons/fa'
+import {RiMovieLine} from 'react-icons/ri'
 
 
 const FilmPage = (props) => {
-  const { title, synopsis } = props.pageContext.content
+  const {	title,	synopsis,director_bio,	director,	duration_mins, language,release_date,	location,  director_img_url, 	other_info,	film_img_url,	tnff_year,	category} = props.pageContext.content
 
 	return (
-		<Fragment>
+		<div className="wrapper">
 
-      <h1>{title}</h1>
-      <p>{synopsis }</p>
-		</Fragment>
+			<h1>{title}</h1>
+			<hr />
+			<div className="film_meta">
+				<p><BiWorld/>{location}</p>
+				<p><BsCalendar />{tnff_year}</p>
+				<p><AiOutlineClockCircle/> {duration_mins} mins</p>
+				<p><FaLanguage/> {language}</p>
+				<p><RiMovieLine />{ category}</p>
+			</div>
+
+			<img src={film_img_url } alt={title } className="film_img"/>
+
+			<div className="film_section">
+				<h3>Synopsis</h3>
+				<p>{synopsis }</p>
+			</div>
+
+			<div className="film_section director">
+				<div className="director_img"><img src={ director_img_url} alt={director } /></div>
+				<div className="director_data">
+					<h3>Director</h3>
+					<h4><BsPersonFill />{ director}</h4>
+					<p>{director_bio }</p>
+				</div>
+			</div>
+
+		</div>
 	)
 };
 
