25c20bf7c07256d22c198fc26a9231a8fa42faec
Sonal Ranjit <sonal.ranjit3@gmail.com>
2021-05-23 10:46:13 -0400

    update cypress test to use 9000 port

--- cypress/integration/about_spec.js
+++ cypress/integration/about_spec.js
@@ -1,7 +1,7 @@
 describe('About', () => {
     it('users should be able to view the "/about" page', () => {
       cy
-        .visit('localhost:8000/about')
+        .visit('localhost:9000/about')
         .get('h1').contains('About TNFF')
         .get('p').contains('Toronto Nepali Film Festival (TNFF) is a not for profit organization based in Toronto, Ontario. Partnering with organizations in Canada, Nepal and beyond, TNFF brings a festival of extraordinary Nepali films in Toronto and other cities. By featuring films of South Asia, specifically Nepal, TNFF intends to add a fresh dimension to the multi-layered diasporic narratives of Canada.');
     });
--- cypress/integration/contact_spec.js
+++ cypress/integration/contact_spec.js
@@ -1,7 +1,7 @@
 describe('Contact', () => {
     it('users should be able to view the "/contact" page', () => {
       cy
-        .visit('localhost:8000/contact')
+        .visit('localhost:9000/contact')
         .get('h1').contains('Contact')
         .get('h4').contains('Email us: tnffcanada@gmail.com')
     });
