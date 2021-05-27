c9f50877781a88ad3c762704ad0aebebe4e8d197
Sonal Ranjit <sonal.ranjit3@gmail.com>
2021-05-26 22:45:07 -0400

    updated message for 404 and films page (#33)

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ const NotFoundPage = () => {
   return (
     <Layout>
       <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>Oops!, You just hit a page that doesn&#39;t exist... the sadness.</p>
     </Layout>
   );
 };
