4ec284fc215ebaaa3ab45a2351f6c49a8cad64a5
Sonal Ranjit <sonal.ranjit3@gmail.com>
2021-12-30 23:28:34 -0500

    update cypress tests with new changes

---
+++ cypress/integration/about_us_spec.js
@@ -0,0 +1,8 @@
+describe('About Us', () => {
+    it('users should be able to view the "/about" page', () => {
+      cy
+        .visit('/about')
+        .get('h1').contains('About Us')
+        .get('h4').contains('Curatorial Committee Selection Criteria')
+    });
+  });
